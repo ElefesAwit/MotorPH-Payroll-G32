16bf4f1380ba48a5d1898b0001dd0c93a879bb0b
Ramones Capulong <20247202@s.ubaguio.edu>
2026-03-05 09:24:23 +0800

    Added system structure

--- src/MotorPHPayroll.java
+++ src/MotorPHPayroll.java
@@ -1,12 +1,107 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+MotorPH Payroll System
+Team Repository: MotorPH-Payroll-G32
 
-/**
- *
- * @author ramon
- */
+IMPORTANT:
+Each member ONLY edits the method assigned to them.
+=====================================================================
+>>>Paki code na lang yung bawat parts natin sa ilalim guys, nilagyan ko
+na ng pangalan base sa naka assign per member, thanks!<<<
+=====================================================================
+*/
+
 public class MotorPHPayroll {
     
+    //wag aalisin guys, ito na gamitin nating VARIABLES
+    static double totalRegularHours;
+    static double overtimeHours;
+    static double lateHours;
+    static double absenceHours;
+
+    static double hourlyRate;
+    static double grossWage;
+    static double netWage;
+    public static void main(String[] args) {
+
+        // ===== PROGRAM FLOW =====
+        loadEmployees();
+        loadAttendance();
+        computeHours();
+        calculateGrossWage();
+        calculateNetSalary();
+        displayPayroll();
+
+    }
+
+    // ============================================
+    // Assigned: Ehric Jason Esmores
+    // Task: Employee Information Overview
+    // PLACE YOUR CODE BELOW
+    // ============================================
+    public static void loadEmployees() {
+
+        // >>> WRITE YOUR CODE HERE <<<
+        
+        
+    }
+
+    // ============================================
+    // Assigned: Daisy Rose Sabuero
+    // Task: Read Attendance Records
+    // PLACE YOUR CODE BELOW
+    // ============================================
+    public static void loadAttendance() {
+
+        // >>> WRITE YOUR CODE HERE <<<
+        
+        
+    }
+
+    // ============================================
+    // Assigned: Daisy Rose Sabuero
+    // Task: Compute Employee Working Hours
+    // PLACE YOUR CODE BELOW
+    // ============================================
+    public static void computeHours() {
+
+        // >>> WRITE YOUR CODE HERE <<<
+        
+        
+    }
+
+    // ============================================
+    // Assigned: Elefes Ramones Capulong
+    // Task: Calculate Employee Gross Wage
+    // PLACE YOUR CODE BELOW
+    // ============================================
+    public static void calculateGrossWage() {
+
+        // >>> WRITE YOUR CODE HERE <<<
+        
+        
+    }
+
+    // ============================================
+    // Assigned: Crysquei Aiko Abella
+    // Task: Calculate Employee Net Wage
+    // PLACE YOUR CODE BELOW
+    // ============================================
+    public static void calculateNetSalary() {
+
+        // >>> WRITE YOUR CODE HERE <<<
+        
+        
+    }
+
+    // ============================================
+    // Assigned: Ehric Jason Esmores
+    // Task: Display Payroll Summary
+    // PLACE YOUR CODE BELOW
+    // ============================================
+    public static void displayPayroll() {
+
+        // >>> WRITE YOUR CODE HERE <<<
+        
+        
+    }
 }
